dc344c0219020a5360fe982a3dde80b36550c04d
Ajay vishwakarma <71971326+ajay776@users.noreply.github.com>
2023-04-01 16:32:06 +0530

    added comment

--- csapp/src/serializer.py
+++ csapp/src/serializer.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import PlayStoreApp
 
 class PlayStoreSerializer(serializers.ModelSerializer):
+#        Serailizer  for PlayStoreApp Model
        class Meta:
               model = PlayStoreApp
               fields = '__all__'
